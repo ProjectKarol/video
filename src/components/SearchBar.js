9327cfc990fed779f85c7cdc88d4cce731831043
Karol Szczesny <karolszczesny@outlook.com>
2019-01-26 21:28:40 +0100

    finish video

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = {
+    term: ""
+  };
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+  onFormSubmit = event => {
+    event.preventDefault();
+    // to d
+    this.props.ofFormSubmit(this.state.term);
+  };
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Search Bar </label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
+// AIzaSyBihCvRh3VVQmTgBSXUUw8lTLkuasaejms
